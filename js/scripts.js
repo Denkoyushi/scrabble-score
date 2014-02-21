a3502d07d8bada247035d47aff7a9c6aa20928f1
Michael Grossman & Jeremy Clemans <student@epicodus.com>
2014-02-21 10:17:46 -0800

    add final tests and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var scrabbleScore = function(word) {
   var finalScore = 0;
   var onePointArray = ["A","E","I","O","U","L","N","R","S","T"];
+  var twoPointArray = ["D","G"];
   var threePointArray = ["B","C","M","P"];
+  var fourPointArray = ["F","H","V","W","Y"];
   var splitWord = word.toUpperCase().split('');
   
   for (var i = 0; i < splitWord.length; i++) {
@@ -11,11 +13,30 @@ var scrabbleScore = function(word) {
         finalScore += 1;
       }
     }
-    for (var k = 0; k < threePointArray.length; k++) {
-      if (letter === threePointArray[k]) {
+    for (var k = 0; k < twoPointArray.length; k++) {
+      if (letter === twoPointArray[k]) {
+        finalScore += 2;
+      } 
+    }
+    for (var l = 0; l < threePointArray.length; l++) {
+      if (letter === threePointArray[l]) {
         finalScore += 3;
       }
-    } 
+    }
+    for (var m = 0; m < fourPointArray.length; m++) {
+      if (letter === fourPointArray[m]) {
+        finalScore += 4;
+      }
+    }
+    if (letter === "K") {
+      finalScore += 5;
+    }
+    if (letter === "J" || letter === "X") {
+      finalScore += 8;
+    }
+    if (letter === "Q" || letter === "Z") {
+      finalScore += 10;
+    }
   }
   return finalScore;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -7,10 +7,30 @@ describe('scrabbleScore', function() {
   it('returns a score of 2 for the word "AT"', function() {
     scrabbleScore("AT").should.equal(2);
   });
-  it('returns a score of 6 for the word "POUR', function() {
+
+  it('returns a score of 6 for the word "POUR"', function() {
     scrabbleScore("POUR").should.equal(6);
   });
-  
+
+  it('returns a score of 5 for the word "DOG"', function() {
+    scrabbleScore("DOG").should.equal(5);
+  });
+
+  it('returns a score of 9 for the word "WOW"', function() {
+    scrabbleScore("WOW").should.equal(9);
+  });
+
+  it('returns a score of 16 for the word "KAYAK"', function() {
+    scrabbleScore("KAYAK").should.equal(16); 
+  });
+
+  it('returns a score of 19 for the word "XEROX"', function() {
+    scrabbleScore("XEROX").should.equal(19); 
+  });
+
+  it('returns a score of 13 for the word "ZERO"', function() {
+    scrabbleScore("ZERO").should.equal(13); 
+  });
 
 });
 
